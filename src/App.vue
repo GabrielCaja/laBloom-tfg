0f77350d8f6025a4172ac638ef2407cf72b43d39
GabrielCaja <gabrielcajaegido@gmail.com>
2025-01-27 12:19:13 +0100

    Actualizar la estructura de la aplicación: agregar Navbar, contenido principal y footer; eliminar estilos CSS no utilizados.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-
-<h1>PAGINA EN CONSTRUCCION</h1>
-
+  <nav><Navbar /></nav>
+  <main>
+    <p>Esto es un main</p>
+  </main>
+  <footer>
+    <p>Esto es un footer</p>
+  </footer>
 </template>
 
-
-<script lang="ts" setup> 
-
+<script lang="ts" setup>
+import { ref } from 'vue'
+import Navbar from './components/Navbar.vue'
 </script>
 
-
-
-<style scoped>
-
-
-</style>
+<style scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>esto es un navbar</h1>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
