3d35175d7c8993cd7c1585697509e334fb59d99d
GabrielCaja <gabrielcajaegido@gmail.com>
2025-02-11 19:49:52 +0100

    Agregar funcionalidad de búsqueda de productos y mejorar la presentación de la navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <Navbar />
-  <div class="grid grid-cols-1 gap-4 p-12 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+  <Navbar @search-productos="searchProductos" />
+  <div
+    v-if="filteredProducts.length"
+    class="grid grid-cols-1 gap-4 p-12 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+  >
     <Card
-      v-for="producto in productos"
+      v-for="producto in filteredProducts"
       :key="producto.id"
       :nombre="producto.nombre"
       :descripcion="producto.descripcion"
@@ -10,6 +13,7 @@
       :imagen="producto.imagen"
     />
   </div>
+  <div v-else class="p-12 text-center">Sin resultados</div>
   <Footer />
 </template>
 
@@ -17,10 +21,11 @@
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
 import Card from './components/Card.vue'
-import { onMounted, ref } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import axios from 'axios'
 
 const productos = ref([])
+const searchQuery = ref('')
 
 onMounted(() => {
   console.log('Recopilando datos...')
@@ -29,6 +34,16 @@ onMounted(() => {
     productos.value = response.data
   })
 })
+const filteredProducts = computed(() => {
+  if (!searchQuery.value) return productos.value
+  return productos.value.filter((producto) =>
+    producto.nombre.toLowerCase().includes(searchQuery.value.toLowerCase()),
+  )
+})
+
+const searchProductos = (query) => {
+  searchQuery.value = query
+}
 </script>
 
 <style scoped></style>
